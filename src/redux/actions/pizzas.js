a45675d6dccbc4fe3c5a110215c03f6c6250f300
scripac <mukhammadiev013@gmail.com>
2021-11-30 10:01:23 +0500

    added readme file

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -10,6 +10,8 @@ export const fetchPizzas = (sortBy, category) => dispatch => {
   dispatch(setLoaded(false))
   axios.get(`/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&order=${sortBy.order}`)
     .then(({data}) => dispatch(setPizzas(data)))
+  // axios.get('https://react-pizza-6ada5-default-rtdb.asia-southeast1.firebasedatabase.app/pizzas.json')
+  //   .then(({data}) => dispatch(setPizzas(data)))
 }
 
 export const setPizzas = items => ({
