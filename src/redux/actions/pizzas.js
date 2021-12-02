71ff705b3e394f0a30c0a8a1d0aad3ee5b338fa1
scripac <mukhammadiev013@gmail.com>
2021-12-02 09:56:28 +0500

    ...

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -6,17 +6,17 @@ export const setLoaded = value => ({
   payload: value
 })
 
-// export const fetchPizzas = (sortBy, category) => dispatch => {
-//   dispatch(setLoaded(false))
-//   axios.get(`/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&order=${sortBy.order}`)
-//     .then(({data}) => dispatch(setPizzas(data)))
-// }
-
-export const fetchPizzas = () => dispatch => {
+export const fetchPizzas = (sortBy, category) => dispatch => {
   dispatch(setLoaded(false))
-  axios.get('https://react-pizza-6ada5-default-rtdb.asia-southeast1.firebasedatabase.app/pizzas.json')
+  axios.get(`/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&order=${sortBy.order}`)
     .then(({data}) => dispatch(setPizzas(data)))
 }
+
+// export const fetchPizzas = () => dispatch => {
+//   dispatch(setLoaded(false))
+//   axios.get('https://react-pizza-6ada5-default-rtdb.asia-southeast1.firebasedatabase.app/pizzas.json')
+//     .then(({data}) => dispatch(setPizzas(data)))
+// }
 
 export const setPizzas = items => ({
   type: SET_PIZZAS,
